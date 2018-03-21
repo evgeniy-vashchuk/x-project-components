be169db9dbecffba79c0215e2ae6fa3c38042bd0
Evgeniy Vashchuk <evgeniy.vashchuk.mail@gmail.com>
2018-03-22 00:25:50 +0200

    add - width of scrollbar

--- x-project-components.js
+++ x-project-components.js
@@ -87,6 +87,22 @@
 		});
 
 		// =========================
+		// WIDTH OF SCROLLBAR =================================
+		// =========================
+
+		var widthOfScrollbar;
+
+		function getScrollBarWidth () {
+			var $outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
+				widthWithScroll = $('<div>').css({width: '100%'}).appendTo($outer).outerWidth();
+			$outer.remove();
+			widthOfScrollbar = 100 - widthWithScroll;
+			return 100 - widthWithScroll;
+		};
+
+		getScrollBarWidth();
+
+		// =========================
 		// STANDARD GOOGLE MAP =================================
 		// =========================
 
